fbb4d92ee7c739613b5987dcbe88f4442daf05cc
Mengjie Chen <mengjiechen1989@icloud.com>
2016-11-14 15:28:22 +0800

    缓存图片

--- 异步绘制圆形头像/异步绘制圆形头像/ViewController.m
+++ 异步绘制圆形头像/异步绘制圆形头像/ViewController.m
@@ -10,9 +10,12 @@
 #import "UIImage+Extension.h"
 #import <Masonry.h>
 
+static NSString *imgCell = @"imgCell";
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource>
+@property (nonatomic,strong) UIImage *img;
+@property (nonatomic,weak)UITableView *tableView;
+@property (nonatomic,assign)BOOL flag;
 
 @end
 
@@ -22,22 +25,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    self.tableView = tableView;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
     
-    [self.view addSubview:imgView];
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:imgCell];
     
-    UIImage *img = [UIImage imageNamed:@"avatar.jpg"];
-    
-    [img mj_cornerImageWithSize:imgView.bounds.size fillColor:[UIColor whiteColor] completion:^(UIImage *image) {
-        imgView.image = img;
-    }];
-    
+
     //自动布局
-    [imgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.height.offset(200);
-        make.center.offset(0);
+    [tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.offset(0);
+        make.top.equalTo(self.mas_topLayoutGuideBottom);
+        make.bottom.equalTo(self.mas_bottomLayoutGuideTop);
     }];
+    
     
 }
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:imgCell forIndexPath:indexPath];
+    if (self.flag == YES) {
+        cell.imageView.image = self.img;
+    }
+    //模拟数据获取
+    UIImage *image = [UIImage imageNamed:@"avatar.jpg"];
+    
+    CGSize imgSize = CGSizeMake(80, 80);
+    if (self.flag == NO) {
+        [image mj_cornerImageWithSize:imgSize fillColor:[UIColor whiteColor] completion:^(UIImage *image) {
+            //缓存图片
+            self.img = image;
+            self.flag = YES;
+            [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+            
+        }];
+    }
+    self.flag = NO;
+    return cell;
+}
 @end
