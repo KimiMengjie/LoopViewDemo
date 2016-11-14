606b6bdfcf59252a8010bde606b3ed7951049afc
Mengjie Chen <mengjiechen1989@icloud.com>
2016-11-14 17:25:09 +0800

    添加headview

--- UI-顶部放大效果Demo/UI-顶部放大效果Demo/MJHeaderViewController.m
+++ UI-顶部放大效果Demo/UI-顶部放大效果Demo/MJHeaderViewController.m
@@ -10,6 +10,7 @@
 #import "HMObjcSugar.h"
 
 NSString *const cellID = @"cellID";
+#define kHeaderHigh 200 //固定高度
 
 @interface MJHeaderViewController ()<UITableViewDelegate,UITableViewDataSource>
 
@@ -24,15 +25,42 @@ NSString *const cellID = @"cellID";
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self prepareTableView];
+    [self prepareHeaderView];
+    
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    //取消自动调整滚动视图间距- ViewController+Nav会自动调整tableView的contentInset
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    //隐藏导航栏
+    [self.navigationController setNavigationBarHidden:YES];
+
+}
 
 /**
- 设置顶部视图
+ 准备顶部视图
  */
 - (void)prepareHeaderView
 {
+    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.hm_width, kHeaderHigh)];
     
+    headView.backgroundColor = [UIColor hm_colorWithHex:0x330000];
+    
+    [self.view addSubview:headView];
+}
+
+/**
+ 设置状态栏
+
+ @return <#return value description#>
+ */
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)prepareTableView{
@@ -44,6 +72,10 @@ NSString *const cellID = @"cellID";
     
     [self.view addSubview:tableView];
     
+    //设置表格的间距
+    tableView.contentInset = UIEdgeInsetsMake(kHeaderHigh, 0, 0, 0);
+    //设置滚动指示器的间距
+    tableView.scrollIndicatorInsets = tableView.contentInset;
 }
 
 - (void)didReceiveMemoryWarning {
