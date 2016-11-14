a37fc32cebc963515e20d6cca21d8ea7ec80fa82
Mengjie Chen <mengjiechen1989@icloud.com>
2016-11-14 20:46:17 +0800

    完成顶部放大效果

--- UI-顶部放大效果Demo/UI-顶部放大效果Demo/MJHeaderViewController.m
+++ UI-顶部放大效果Demo/UI-顶部放大效果Demo/MJHeaderViewController.m
@@ -10,6 +10,8 @@
  */
 #import "MJHeaderViewController.h"
 #import "HMObjcSugar.h"
+#import <AFNetworking/AFNetworking.h>
+#import <YYWebImage/YYWebImage.h>
 
 NSString *const cellID = @"cellID";
 #define kHeaderHigh 200 //固定高度
@@ -19,6 +21,12 @@ NSString *const cellID = @"cellID";
 @end
 
 @implementation MJHeaderViewController
+{
+    UIView *_headView;
+    UIImageView *_headImgView;
+    UIView *_lineView;
+    UIStatusBarStyle _statusBarStyle;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -29,6 +37,7 @@ NSString *const cellID = @"cellID";
     [self prepareTableView];
     [self prepareHeaderView];
     
+    _statusBarStyle = UIStatusBarStyleLightContent;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -48,11 +57,32 @@ NSString *const cellID = @"cellID";
  */
 - (void)prepareHeaderView
 {
-    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.hm_width, kHeaderHigh)];
+    _headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.hm_width, kHeaderHigh)];
     
-    headView.backgroundColor = [UIColor hm_colorWithHex:0x330000];
+    _headView.backgroundColor = [UIColor hm_colorWithHex:0xf8f8f8];
     
-    [self.view addSubview:headView];
+    [self.view addSubview:_headView];
+    
+    _headImgView = [[UIImageView alloc] initWithFrame:_headView.bounds];
+    //设置等比例填充contentMode
+    _headImgView.contentMode = UIViewContentModeScaleAspectFill;
+    //设置图像裁切
+    _headImgView.clipsToBounds = YES;
+    
+    _headImgView.backgroundColor = [UIColor hm_colorWithHex:0x00033];
+    
+    [_headView addSubview:_headImgView];
+    
+    //添加分割线1像素线
+    CGFloat lineHeight = 1 / [UIScreen mainScreen].scale;
+    _lineView = [[UIView alloc] initWithFrame:CGRectMake(0,kHeaderHigh - lineHeight, _headView.hm_width, lineHeight)];
+    _lineView.backgroundColor = [UIColor lightGrayColor];
+    
+    [_headView addSubview:_lineView];
+    // 设置图像
+    NSURL *url = [NSURL URLWithString:@"http://www.who.int/entity/campaigns/immunization-week/2015/large-web-banner.jpg?ua=1"];
+    
+    [_headImgView yy_setImageWithURL:url options:YYWebImageOptionShowNetworkActivity];
 }
 
 /**
@@ -62,7 +92,7 @@ NSString *const cellID = @"cellID";
  */
 -(UIStatusBarStyle)preferredStatusBarStyle
 {
-    return UIStatusBarStyleLightContent;
+    return _statusBarStyle;
 }
 
 - (void)prepareTableView{
@@ -83,6 +113,39 @@ NSString *const cellID = @"cellID";
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat offset = scrollView.contentOffset.y + scrollView.contentInset.top;
+
+    if (offset <= 0) {
+
+        //调整headView和headImageView
+        _headView.hm_y = 0;
+        //保持高度一致
+        _headView.hm_height = kHeaderHigh - offset;
+        _headImgView.alpha = 1;
+    }else{
+
+        _headView.hm_height = kHeaderHigh;
+
+        //headerView最小的y值
+        CGFloat min = kHeaderHigh - 64;
+        _headView.hm_y = -MIN(min, offset);
+        //设置透明度,当offset /min == 1的不可见
+        CGFloat progress = 1 - (offset / min);
+        _headImgView.alpha = progress;
+        //根据透明的修改状态栏颜色
+        _statusBarStyle = (progress < 0.5) ? UIStatusBarStyleDefault : UIStatusBarStyleLightContent;
+        //主动更新状态
+        [self.navigationController setNeedsStatusBarAppearanceUpdate];
+    }
+    //设置图像高度
+    _headImgView.hm_height = _headView.hm_height;
+    //设置分割线的位置
+    _lineView.hm_y = _headView.hm_height - _lineView.hm_height;
 }
 
 #pragma mark - UITableViewDelegate,UITableViewDataSource
